bea349911dd1eecc1260b8accaeca562ec6de5bf
AlexandruGH <alex_dodo_gh@yahoo.com>
2015-03-07 21:46:26 +0200

    I have a bug that I don't know to fix. The program has no errors or warnings but the output.txt file isn't correct written.

---
+++ 30411/Ghiurutan V. Alexandru/main.c
@@ -0,0 +1,274 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct node
+{
+    int code;
+    struct node *next;
+} nodeT;
+
+nodeT *head;
+nodeT *tail;
+
+void addfirst(int givencode)
+{
+    if(head==NULL)
+    {
+        head=(nodeT *)malloc(sizeof(nodeT));
+        head->next=NULL;
+        head->code=givencode;
+        tail=head;
+    }
+    else
+    {
+        nodeT *p=(nodeT *)malloc(sizeof(nodeT));
+        p->code=givencode;
+        p->next=head;
+        head=p;
+    }
+}
+void addlast(int givencode)
+{
+    if(head==NULL)
+    {
+        head=(nodeT *)malloc(sizeof(nodeT));
+        head->code=givencode;
+        head->next=NULL;
+        tail=head;
+    }
+    else
+    {
+        nodeT *p=(nodeT *)malloc(sizeof(nodeT));
+        p->code=givencode;
+        p->next=NULL;
+        tail->next=p;
+        tail=p;
+    }
+}
+void deletelast()
+{
+    nodeT *q,*q1;
+    q=head;
+    q1=NULL;
+    if(q!=NULL)
+    {
+        while(q!=tail)
+        {
+            q1=q;
+            q=q->next;
+        }
+        if(q==head)
+        {
+            //One node.
+            head=tail=NULL;
+        }
+        else
+        {
+            q1->next=NULL;
+            tail=q1;
+            free(q);
+        }
+    }
+}
+void deletefirst()
+{
+    nodeT *q;
+    if(head!=NULL)
+    {
+        q=head;
+        head=head->next;
+        free(q);
+        if(head==NULL)
+        {
+            tail=NULL;
+        }
+    }
+}
+void doom_the_list()
+{
+    nodeT *q;
+    while(head!=NULL)
+    {
+        q=head;
+        head=q->next;
+        free(q);
+    }
+}
+void remove_given(int givenkey)
+{
+    nodeT *q,*q1;
+    q=head;
+    q1=NULL;
+    while(q!=NULL)
+    {
+        if(q->code==givenkey)
+        {
+            break;
+        }
+        q1=q;
+        q=q->next;
+    }
+    if(q!=NULL)
+    {
+        if(q==head)
+        {
+            head=head->next;
+            free(q);
+            if(head==NULL)
+            {
+                tail=NULL;
+            }
+        }
+        else
+        {
+            q1->next=q->next;
+            if(q==tail)
+            {
+                tail=q1;
+            }
+            free(q);
+        }
+    }
+}
+void print_all(FILE *pf)
+{
+    nodeT *q;
+    q=head;
+    while(q!=NULL)
+    {
+        fprintf(pf,"%d ",q->code);
+        q=q->next;
+    }
+    if(head!=NULL)
+    {
+        fprintf(pf,"\n");
+    }
+}
+void print_first_x(FILE *pf,int x)
+{
+    nodeT *q;
+    int nr=0;
+    q=head;
+    while(q!=NULL)
+    {
+        if(nr==x)
+        {
+            break;
+        }
+        fprintf(pf,"%d ",q->code);
+        q=q->next;
+        nr++;
+    }
+    if(head!=NULL)
+    {
+        fprintf(pf,"\n");
+    }
+}
+void print_last_x(FILE *pf,int x)
+{
+    nodeT *q;
+    int nr=0;
+    q=head;
+    if(q!=NULL)
+    {
+        while(q!=NULL)
+        {
+            q=q->next;
+            nr++;
+        }
+        if(x>=nr)
+        {
+            print_all(pf);
+        }
+        else
+        {
+            int p=nr-x;
+            int y=0;
+            q=head;
+            while(y!=p)
+            {
+                q=q->next;
+                y++;
+            }
+            while(q!=NULL)
+            {
+                fprintf(pf,"%d ",q->code);
+                q=q->next;
+            }
+            fprintf(pf,"\n");
+        }
+    }
+}
+
+int main()
+{
+    FILE *f=fopen("input.txt","r");
+    if(f==NULL)
+    {
+        perror("Can't open file.");
+        exit(1);
+    }
+    FILE *pf=fopen("output.txt","w");
+    if(pf==NULL)
+    {
+        perror("Can't open file.");
+        exit(1);
+    }
+    char s[20];
+    fseek(f,0L,SEEK_SET);
+    while(fgets(s,sizeof(s),f)!=NULL)
+    {
+        int x;
+        char *p=strtok(s," ");
+        if(strcmp(p,"AF")==0)
+        {
+            p=strtok(NULL,"\0");
+            sscanf(p,"%d",&x);
+            addfirst(x);
+        }
+        if(strcmp(p,"AL")==0)
+        {
+            p=strtok(NULL,"\0");
+            sscanf(p,"%d",&x);
+            addlast(x);
+        }
+        if(strcmp(p,"DF")==0)
+        {
+            deletefirst();
+        }
+        if(strcmp(p,"DL")==0)
+        {
+            deletelast();
+        }
+        if(strcmp(p,"DOOM_THE_LIST")==0)
+        {
+            doom_the_list();
+        }
+        if(strcmp(p,"DE")==0)
+        {
+            p=strtok(NULL,"\0");
+            sscanf(p,"%d",&x);
+            remove_given(x);
+        }
+        if(strcmp(p,"PRINT_ALL")==0)
+        {
+            print_all(pf);
+        }
+        if(strcmp(p,"PRINT_F")==0)
+        {
+            p=strtok(NULL,"\0");
+            sscanf(p,"%d",&x);
+            print_first_x(pf,x);
+        }
+        if(strcmp(p,"PRINT_L")==0)
+        {
+            p=strtok(NULL,"\0");
+            sscanf(p,"%d",&x);
+            print_last_x(pf,x);
+        }
+    }
+    fclose(f);
+    fclose(pf);
+    return 0;
+}
