1fc6c672eea633754e523e93a4c483e9f2ffa542
bbeniamin <beniaminbucur@gmail.com>
2015-03-10 22:57:05 +0200

    Versiune corectata

--- 30414/Bucur E. Beniamin Eugen/tema1/main.c
+++ 30414/Bucur E. Beniamin Eugen/tema1/main.c
@@ -12,24 +12,22 @@ int main()
     {
         if (strcmp(instr, "AF") == 0)
             ft_AF(x);
-        if (strcmp(instr, "AL") == 0)
+        else if (strcmp(instr, "AL") == 0)
             ft_AL(x);
-        if (strcmp(instr, "DF") == 0)
+        else if (strcmp(instr, "DF") == 0)
             ft_DF();
-        if (strcmp(instr, "DL") == 0)
+        else if (strcmp(instr, "DL") == 0)
             ft_DL();
-        if (strcmp(instr, "DOOM_THE_LIST") == 0)
+        else if (strcmp(instr, "DOOM_THE_LIST") == 0)
             ft_DOOM_THE_LIST();
-        if (strcmp(instr, "DE") == 0)
+        else if (strcmp(instr, "DE") == 0)
             ft_DE(x);
-        if (strcmp(instr, "PRINT_ALL") == 0)
+        else if (strcmp(instr, "PRINT_ALL") == 0)
             ft_PRINT_ALL();
-        if (strcmp(instr, "PRINT_F") == 0)
+        else if (strcmp(instr, "PRINT_F") == 0)
             ft_PRINT_F(x);
-        if (strcmp(instr, "PRINT_L") == 0)
+        else if (strcmp(instr, "PRINT_L") == 0)
             ft_PRINT_L(x);
-        print_list();
-        printf ("\n");
     }
     return 0;
 }
