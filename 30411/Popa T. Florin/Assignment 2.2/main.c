2da83ecd23fbdbfc8cb666c5c55222deff61b63a
Florin Popa <pflorin17@gmail.com>
2015-03-16 18:24:20 +0200

    Second assignment.

--- 30411/Popa T. Florin/Assignment 2.2/main.c
+++ 30411/Popa T. Florin/Assignment 2.2/main.c
@@ -3,49 +3,63 @@
 #include <strings.h>
 #include <stdbool.h>
 
-typedef struct node{
-char name[30];
-int time;
-int money;
-struct node *next;}node;
+typedef struct node
+{
+    char name[30];
+    int time;
+    int money;
+    struct node *next;
+} node;
 node *head;
 FILE *f;
 FILE *g;
 
-void queue(char n[30],int r,int s){
-    if(head==NULL){
+void queue(char n[30],int r,int s)
+{
+    if(head==NULL)
+    {
         head=(node*)malloc(sizeof(node));
         strcpy(head->name,n);
         head->time=s;
         head->money=r;
         head->next=NULL;
-    }else{
-    node *aux;
-    node *temp=head;
-    aux=(node*)malloc(sizeof(node));
-    while(temp->next!=NULL){
-        temp=temp->next;
     }
-    temp->next=aux;
-    strcpy(aux->name,n);
-    aux->time=s;
-    aux->money=r;
-    aux->next=NULL;
+    else
+    {
+        node *aux;
+        node *temp=head;
+        aux=(node*)malloc(sizeof(node));
+        while(temp->next!=NULL)
+        {
+            temp=temp->next;
+        }
+        temp->next=aux;
+        strcpy(aux->name,n);
+        aux->time=s;
+        aux->money=r;
+        aux->next=NULL;
     }
 }
 
-void cashier(int x){
+void cashier(int x)
+{
     node *temp=head;
     int sec=0;
     int mon=0;
-    while(temp!=NULL){
-            if(temp->time+sec<=x){
+    while(temp!=NULL)
+    {
+        if(temp->time+sec<=x)
+        {
             sec=sec+temp->time;
             mon=mon+temp->money;
-            }else{break;}
-            temp=temp->next;
-            }
-fprintf(f,"After %d seconds: %d\n",x,mon);
+        }
+        else
+        {
+            break;
+        }
+        temp=temp->next;
+    }
+    fprintf(f,"After %d seconds: %d\n",x,mon);
 }
 
 bool isNumeric(const char *str)
@@ -59,31 +73,39 @@ bool isNumeric(const char *str)
     return true;
 }
 
-void read(){
+void read()
+{
     char line[100];
     int a[100],n=0;
     int money;
     int time;
     g=fopen("input.dat","r");
-    if(g==NULL){
+    if(g==NULL)
+    {
         printf("Error! Could not open file!\n");
-    }else{
-        while(fscanf(g,"%s",line)!=EOF){
+    }
+    else
+    {
+        while(fscanf(g,"%s",line)!=EOF)
+        {
 
-                if(isNumeric(line)==true)
-                {
-                    a[n]=atoi(line);
-                    n++;
-                }else{
+            if(isNumeric(line)==true)
+            {
+                a[n]=atoi(line);
+                n++;
+            }
+            else
+            {
                 fscanf(g,"%d",&money);
                 fscanf(g,"%d",&time);
                 queue(line,money,time);
-                }
-        }
-}
-            for(int i=0;i<n;i++){
-                cashier(a[i]);
             }
+        }
+    }
+    for(int i=0; i<n; i++)
+    {
+        cashier(a[i]);
+    }
 }
 
 int main()
