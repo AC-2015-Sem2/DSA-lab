e2bd4b2ec0bb8559e34a2b0fd3ed0ab3a521ec9c
stefaniasucitu <sucitu_stefania@yahoo.com>
2015-03-30 20:45:42 +0300

    here is homework 3.

---
+++ 30411/Sucitu G. Stefania/assignment3/main.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef struct nod
+{
+    int data;
+    struct nod *left;
+    struct nod *right;
+} nodet;
+
+typedef struct queue
+{
+    int dat;
+    struct queue *next;
+    struct queue *previous;
+} node;
+
+node *head,*tail;
+
+nodet*createnode(int data)
+{
+
+    nodet *p;
+    p=(nodet*)malloc(sizeof(nodet));
+    p->data=data;
+    p->right=p->left=NULL;
+    return p;
+}
+
+nodet* createbintree( )
+{
+    nodet* p;
+    char* content=(char*)malloc(sizeof(char)*20);
+    scanf("%s",content);
+    if (strcmp(content,"*")==0)
+        return NULL;
+    else
+    {
+        p=createnode(atoi(content));
+        p->left=createbintree();
+        p->right=createbintree();
+    }
+    return p;
+}
+
+nodet* createbintree2( )
+{
+    nodet* p;
+    int content=(int*)malloc(sizeof(int)*20);
+    content=head->dat;
+    head=head->next;
+    if (content==8)
+        return NULL;
+    else
+    {
+        p=createnode(content);
+        p->left=createbintree2();
+        p->right=createbintree2();
+    }
+    return p;
+}
+
+void preorder(nodet* root)
+{
+    if (root!=NULL)
+    {
+        AL(root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+    else
+    {
+        AL(8);
+    }
+}
+
+void (*printElement)(nodet*);
+void printIntegerElement(nodet * node);
+
+void printIntegerElement(nodet * node)
+{
+    printf ( "%d " ,(int) node->data );
+}
+
+void prettyPrint(nodet *root,int recLevel)
+{
+    if(root==NULL)
+    {
+        recLevel--;
+        return;
+    }
+    recLevel++;
+    prettyPrint(root->right,recLevel);
+    int j=0;
+    for(j=0; j<recLevel-1; j++)
+    {
+        printf("     ");
+    }
+    printElement(root);
+    printf("\n");
+    prettyPrint(root->left,recLevel);
+}
+
+void AL(int x)
+{
+    if (head==NULL)
+    {
+        head=(struct node*)malloc(sizeof(node));
+        head->dat=x;
+        head->previous=NULL;
+        head->next=NULL;
+        tail=head;
+    }
+    else
+    {
+        node *t=(node*)malloc(sizeof(node));
+        t->dat=x;
+        t->next=NULL;
+        t->previous=tail;
+        tail->next=t;
+        tail=t;
+    }
+}
+
+int main()
+{
+    nodet* root=createbintree();
+    printElement = &printIntegerElement;
+    prettyPrint(root,0);
+    preorder(root);
+    node *t=head;
+    while (t!=NULL)
+    {
+        printf("%d ",t->dat);
+        t=t->next;
+    }
+    printf("\n");
+    createbintree2();
+    prettyPrint(root,0);
+    printf("\n");
+    return 0;
+}
