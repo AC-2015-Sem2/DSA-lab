52a2f9613dcf15f955f1248929a6556412d5468e
Andrei Pop <andreipop95@gmail.com>
2015-04-07 09:51:42 +0300

    AVL Trees - updated code-

--- 30414/Pop I. Andrei/AVL trees/insert-delete/main.c
+++ 30414/Pop I. Andrei/AVL trees/insert-delete/main.c
@@ -4,26 +4,15 @@ int main()
 {
     NodeT *root = NULL;
 
-    root = insertNode(root,10);
-    root = insertNode(root,5);
-    root = insertNode(root,15);
-    root = insertNode(root,14);
-    root = insertNode(root,20);
-    root = insertNode(root,1);
-    root = insertNode(root,8);
-    root = insertNode(root,6);
-    root = insertNode(root,7);
+    int i=0;
+    for(i=0; i<90;i++)
+        root=insertNode(root,i);
 
-
-    root = deleteNode(root, 15);
-    root = deleteNode(root, 14);
-    root = deleteNode(root, 20);
-    root = deleteNode(root, 1);
-    root = deleteNode(root, 7);
-    root = deleteNode(root, 8);
-
+    for(i=20;i<80;i++)
+        root=deleteNode(root, i);
 
     prettyPrint(root, 0);
+
 
     return 0;
 }
