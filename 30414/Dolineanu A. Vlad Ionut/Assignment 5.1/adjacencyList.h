d3037720fd79b65bdafd6628e391d3cea3adbed6
DolineanuVlad <dolineanuvlad@gmail.com>
2015-04-27 15:20:40 +0300

    Finished the first assignment.

---
+++ 30414/Dolineanu A. Vlad Ionut/Assignment 5.1/adjacencyList.h
@@ -0,0 +1,24 @@
+#ifndef ADJACENCYLIST_H_INCLUDED
+#define ADJACENCYLIST_H_INCLUDED
+
+#include "graphRepresentation.h"
+#include "graphTraversals.h"
+#include "graphAlgorithms.h"
+
+typedef struct Node{
+    int content, numberOfNeighbors;
+    int *neighbors;
+    struct Node *next;
+}adjNode;
+
+adjNode *adjacencyList;
+int **adjMatrixFromList;
+
+void addToAdjacencyList(int, int*);
+void printVector(int*, int);
+void matrixToList();
+void printAdjList();
+void listToMatrix();
+void printAdjMatrixFromList();
+
+#endif // ADJACENCYLIST_H_INCLUDED
