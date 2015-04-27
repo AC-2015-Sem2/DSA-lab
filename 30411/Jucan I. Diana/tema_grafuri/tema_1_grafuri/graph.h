19936dbe166d013ea15e61cb2bc8764fd8b7a256
JucanDiana <jucandiana@yahoo.com>
2015-04-27 23:22:12 +0300

    Hello! This is my homework! :)

---
+++ 30411/Jucan I. Diana/tema_grafuri/tema_1_grafuri/graph.h
@@ -0,0 +1,24 @@
+
+
+#include <stdio.h>
+
+#define UNVISITED 0
+#define VISITED 1
+
+void readFromAdjMatrix(FILE * f);
+
+
+int ** adjMatrix;
+int nrOfVerteces;
+
+void printAdjMatrix();
+
+void bfs(int searchNode);
+void dfs(int searchNode);
+void dfsRecurs(int searchNode);
+
+int **adjList;
+int *adjListlength;
+void createAdjList(int ** adjMatrix);
+void printAdjList();
+int **list2matrix(int** adjLists, int* adjListLengths);
