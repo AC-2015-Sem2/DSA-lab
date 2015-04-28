c6df84545a0cec17eb10b6e94b7d7abd81ff13ff
alex.cosma <alexx.cosma@gmail.com>
2015-04-28 21:36:02 +0300

    - added Prim and Dijkstra

--- Interesting Stuff/Graphs101 - complete/graphAlgorithms.c
+++ Interesting Stuff/Graphs101 - complete/graphAlgorithms.c
@@ -4,7 +4,27 @@
 //! Prim related stuff
 edgeT getMinimumEdgeForCurrentlyVisitedNodes(int * visited)
 {
+    int i,j;
+    edgeT minEdge;
+    minEdge.weight=MAX;
 
+    for (i=0; i<nrOfVerteces; i++)
+    {
+        for (j=0; j<nrOfVerteces; j++)
+        {
+            if(visited[i] == VISITED && visited[j] != VISITED && adjMatrix[i][j] > 0)
+            {
+                if(adjMatrix[i][j]<minEdge.weight)
+                {
+                    minEdge.source = i;
+                    minEdge.destination = j;
+                    minEdge.weight = adjMatrix[i][j];
+                }
+            }
+        }
+    }
+
+    return minEdge;
 }
 
 void printEdges(edgeT* edges, int n)
@@ -19,7 +39,31 @@ void printEdges(edgeT* edges, int n)
 
 void prim(int startNode)
 {
+    printf("Prim's Algorithm started\n");
 
+    int nrVertecesVisited=0,i;
+    int * visited = (int*)malloc(nrOfVerteces * sizeof(int));
+    for(i=0; i<nrOfVerteces; i++)
+    {
+        visited[i] = UNVISITED;
+    }
+    int minCost = 0;
+
+    edgeT * edges = (edgeT*) malloc((nrOfVerteces-1) * sizeof(edgeT));
+
+    visited[startNode] = VISITED;
+
+    while(nrVertecesVisited<nrOfVerteces-1)
+    {
+        edgeT minEdge = getMinimumEdgeForCurrentlyVisitedNodes(visited);
+        visited[minEdge.destination]=VISITED;
+        edges[nrVertecesVisited++] = minEdge;
+        minCost+=minEdge.weight;
+    }
+
+    printEdges(edges, nrVertecesVisited);
+    printf("Cost of MST by Prim's Algorithm: %d\n",minCost);
+    printf("Prim's algorithm ended\n\n");
 }
 
 //! Kruskal related stuff
@@ -81,10 +125,65 @@ void kruskal()
 //! Dijsktra related stuff
 
 int getMinDistanceVertex(int * distances, int * visited){
-
+    int min=MAX, i;
+    int minV;
+    for(i=0;i<nrOfVerteces;i++){
+        if(distances[i] < min && visited[i] == UNVISITED){
+            minV = i;
+        }
+    }
+    return minV;
 }
 
 void dijkstra(int startNode)
 {
+    printf("Dijkstra's Algorithm started\n");
+    int * distances = (int*)malloc(nrOfVerteces * sizeof(int));
+    int * previous  = (int*)malloc(nrOfVerteces * sizeof(int));
+    int * visited   = (int*)malloc(nrOfVerteces * sizeof(int));
+    int nrOfVertecesVisited=0,i;
+    for(i=0; i<nrOfVerteces; i++)
+    {
+        distances[i] = MAX;
+        previous[i]  = UNDEFINED;
+        visited[i] = UNVISITED;
+    }
+    distances[startNode] = 0;
 
+    while(nrOfVertecesVisited < nrOfVerteces){
+        int u = getMinDistanceVertex(distances,visited);
+        visited[u] = VISITED;
+
+        int nrOfNeighbors = getNumberOfNeighborsOfVertex(u);
+        int * neighbors = getAllNeighborsOfVertex(u);
+        for(i=0; i<nrOfNeighbors; i++)
+        {
+            int w = neighbors[i];
+            int alt = distances[u] + adjMatrix[u][w];
+            if(alt<distances[w]){
+                distances[w] = alt;
+                previous[w] = u;
+            }
+        }
+        nrOfVertecesVisited++;
+    }
+
+    for(i=0; i<nrOfVerteces; i++){
+        int u=i;
+        if(i!=startNode){
+            while(previous[u] != UNDEFINED){
+                push(u);
+                u = previous[u];
+            }
+
+            printf("Path from %c to %c is: %c ",startNode + 65, i+65, startNode + 65);
+            while(!isEmptyStack()){
+                printf("-> %c ",peekStack()->content + 65);
+                pop();
+            }
+            printf("\n");
+        }
+    }
+
+    printf("Dijkstra's Algorithm ended\n");
 }
