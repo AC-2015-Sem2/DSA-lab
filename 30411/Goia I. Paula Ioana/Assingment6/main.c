56ce0132fc67641fdd189e2ad4581a4d152e07a9
Goia I.PaulaIoana <goiapaulaioana@gmail.com>
2015-05-01 16:46:43 +0300

    Assingment 6

---
+++ 30411/Goia I. Paula Ioana/Assingment6/main.c
@@ -0,0 +1,19 @@
+#include "graphRepresentation.h"
+#include "graphTraversals.h"
+#include "graphAlgorithms.h"
+
+int main()
+{
+    FILE * f = fopen("matrix.txt","r");
+    readFromAdjMatrix(f);
+    printAdjMatrix();
+    bfs(0);
+    dfs(0);
+    dfsRecurs(0);
+    prim(0);
+    dijkstra(0);
+    kruskal();
+    bellman(5);
+
+    return 0;
+}
