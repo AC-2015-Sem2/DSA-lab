6a9e03d547c282db8800f51584e888a4f07122e7
vlcmodan <valentinmodan@yahoo.com>
2015-05-17 15:40:15 +0300

    Open Addresing Update

--- 30414/Modan A. Valentin/Hash Tables - Open Addressing/hashTable.c
+++ 30414/Modan A. Valentin/Hash Tables - Open Addressing/hashTable.c
@@ -6,15 +6,16 @@ void initHashTable(int N)
  int i;
  size=(int)((float)N*INITIAL_HT_SIZE_FACTOR);
  hashTable=(char**)malloc(sizeof(char *)*size);
- for(i=0;i<size;i++)
+ for(i=0;i<size;i++)//AICI AM COMENTAT
     hashTable[i]=NULL;
-    memset(hashTable,NULL,size);
+   // memset(hashTable,NULL,size);
     //! initialize hash table with length "INITIAL_HT_SIZE_FACTOR" of N (1/4 of N)
 }
 
 float getFillFactor()
 {
     //! return the fill factor of the hash table
+   // return (float)hash_number/(float)size;
     float fillFactor = 0;
     int i,nr=0;
     for(i=0;i<size;i++)
@@ -35,11 +36,12 @@ void resizeHashTable()
     char **aux=(char**)malloc(sizeof(char *)*size);
 
     int i;
-    for(i=0;i<size;i++)
+    for(i=0;i<=size;i++)
         aux[i]=hashTable[i];
     size*=2;
     hashTable=(char**)malloc(sizeof(char *)*size);
-    for(i=0;i<size;i++)
+    // memset(hashTable,NULL,size);
+    for(i=0;i<=size;i++)
         hashTable[i]=NULL;
     for(i=0;i<size/2;i++)
         if(aux[i]!=NULL)
@@ -50,6 +52,7 @@ void resizeHashTable()
 
 int insertElement(char * element)
 {
+
     int coliziuni=0;
     int cod;
     if(getFillFactor()>MAX_FILL_FACTOR)
@@ -59,12 +62,12 @@ int insertElement(char * element)
     //! returns the number of collisions which occurred before the element was inserted
 
    cod=hashFunction(element,coliziuni);
-    while(hashTable[cod]!=NULL)
-    {
-        cod++;
+    while(hashTable[cod++]!=NULL)
         coliziuni++;
-        cod%=size;
-    }
+
+
+     cod%=size;
+    // I put this out of the while function
     hashTable[cod]=element;
     return coliziuni;
 }
@@ -73,7 +76,7 @@ int h1(char* content)
 {
     int i=0,sum=0;
     while((content[i])!='\n'&&(content[i])!='\0')
-        sum=(sum+content[i++])%size;
+        sum=(sum+content[i++]);
     return sum%size;
 }
 int h2(char* content)
