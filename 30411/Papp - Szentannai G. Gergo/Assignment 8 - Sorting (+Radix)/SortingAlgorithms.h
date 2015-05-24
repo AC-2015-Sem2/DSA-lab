96fc54494dad9e7b31bb544a61b050fc1f5a1a59
Papp Szentannai Gergő <gergo_13@yahoo.com>
2015-05-24 13:57:03 +0300

    Radix Sortting

---
+++ 30411/Papp - Szentannai G. Gergo/Assignment 8 - Sorting (+Radix)/SortingAlgorithms.h
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <time.h>
+
+enum inPlaceSortingFunctions{
+    BUBBLE = 0,
+    INSERTION = 1,
+    SELECTION = 2,
+    QUICKSORT = 3
+};
+
+enum outOfPlaceSortingFunctions{
+    MERGESORT = 0,
+    RADIX = 1
+};
+
+static const char* inPlaceSortingFunctionNames[] = { "Bubble Sort", "Insertion Sort", "Selection Sort", "Quicksort" };
+static const char* outOfPlaceSortingFunctionNames[] = { "Merge Sort", "Radix Sort" };
+
+//! counters which will be reset after each sorting execution
+long comparisons;
+long assignments;
+
+//! 3 in-place sorting functions (bubble, insertion, selection, quicksort)
+void (*inPlaceSortingFunctions[4])(int *x, int n);
+
+//! 2 out-of-place sorting functions (merge, radix)
+void (*outOfPlaceSortingFunctions[2])(int *a, int n, int *endResult);
+
+void initFunctions();
+
+void resetCounters();
+
+//! in-place sorting functions
+void bubbleSort(int *x, int n);
+void insertionSort(int *x, int n);
+void selectionSort(int *x, int n);
+void quicksort(int *x, int n);
+
+//! out-of-place sorting functions
+void mergeSort(int *a, int n, int *endResult);
+void radixSort(int *a, int n, int *endResult);
+
+
